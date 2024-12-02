6f5ed43048f00ce76c2dbe025fe7f393af39dda8
Navneet Rajput <navneetrajput404@gmail.com>
2024-12-02 23:57:53 +0530

    Time: 6 ms (7.64%), Space: 42.3 MB (6.34%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,17 +1,31 @@
 import java.util.*;
 class Solution {
     public void sortColors(int[] nums) {
-        int temp ;
-        int j ;
-        for(int i = 1 ; i<nums.length ; i++){
-            temp = nums[i];
-            j = i-1;
-            while(j>=0 && nums[j] > temp){
-                nums[j+1]= nums[j];
-                j--;
-            }
-            nums[j+1] = temp;
+        // dutch national flag algorithms
+       int c0 =0, c1=0 , c2 =0;
+       for (int i = 0 ; i<nums.length ; i++){
+        if(nums[i]==0){
+            c0++;
+        }else if (nums[i]==1){
+            c1++;
+        }else{
+            c2++;
         }
+       }
+
+       int idx = 0 ;
+       for(int  i = 0 ; i <c0 ; i ++){
+            nums[idx]=0;
+            idx++;
+       }
+       for(int  i = 0 ; i <c1 ; i ++){
+            nums[idx]=1;
+             idx++;
+       }
+       for(int  i = 0 ; i <c2 ; i ++){
+            nums[idx]=2;
+             idx++;
+       }
         for(int i = 0 ; i<nums.length ; i++){
             System.out.print(nums[i]);
         }
